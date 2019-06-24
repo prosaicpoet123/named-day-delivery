e2c55e90a108db2969061a94e32e90c93b98273e
prosaicpoet123 <prosaicpoet@hotmail.co.uk>
2019-06-24 10:23:57 +0100

    applies missing change

--- src/setupTest.js
+++ src/setupTest.js
@@ -4,14 +4,8 @@ configure({ adapter: new Adapter() });
 
 window.matchMedia = window.matchMedia || function() {
     return {
-<<<<<<< HEAD
         matches : false,
         addListener : function() {},
         removeListener: function() {}
-=======
-    matches : false,
-    addListener : function() {},
-    removeListener: function() {}
->>>>>>> master
     };
 };
