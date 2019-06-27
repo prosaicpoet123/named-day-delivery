d2a4add292c5d1046ad792ad109eca5c05a1efbc
prosaicpoet123 <prosaicpoet@hotmail.co.uk>
2019-06-27 17:18:21 +0100

    feat(HB1-1621): separate webpack in prod and dev build

--- webpack.common.js
+++ webpack.common.js
@@ -4,7 +4,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: "./src/index.js",
-  mode: "development",
   module: {
     rules: [
       {
@@ -50,7 +49,7 @@ module.exports = {
   },
   resolve: { extensions: ["*", ".js", ".jsx"] },
   output: {
-    path: path.resolve(__dirname, "/dist/"),
+    path: path.resolve(__dirname, "dist/"),
     filename: "bundle.js"
   },
   devServer: {
